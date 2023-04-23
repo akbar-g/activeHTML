1a536fa44235e0b8aa136f82adee9f838992637d
HuangGreatd <13393711969@163.com>
2023-04-23 10:10:10 +0800

    组队

--- activeduan/src/components/friend/friends.vue
+++ activeduan/src/components/friend/friends.vue
@@ -18,88 +18,6 @@
     <van-button square type="primary" text="拉黑" @click="friendsNone('拉黑')"/>
   </template>
   </van-swipe-cell>
-  <van-swipe-cell style="margin-bottom: .625rem;margin-left: .625rem;">
-    <van-image
-    round
-    width="2.5rem"
-    height="2.5rem"
-    fit="cover"
-    position="center"
-    src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
-    style="vertical-align: middle;"
-    />
-    <h4 style="display:inline-block;width: 80%;padding-right: 60%;color: black;margin-left: .625rem;">A朋友</h4>
-    <template #right>
-    <van-button square type="danger" text="删除" @click="friendsDel('删除')"/>
-    <van-button square type="primary" text="拉黑" @click="friendsNone('拉黑')"/>
-  </template>
-  </van-swipe-cell>
-  <van-swipe-cell style="margin-bottom: .625rem;margin-left: .625rem;">
-    <van-image
-    round
-    width="2.5rem"
-    height="2.5rem"
-    fit="cover"
-    position="center"
-    src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
-    style="vertical-align: middle;"
-    />
-    <h4 style="display:inline-block;width: 80%;padding-right: 60%;color: black;margin-left: .625rem;">A朋友</h4>
-    <template #right>
-    <van-button square type="danger" text="删除" @click="friendsDel('删除')"/>
-    <van-button square type="primary" text="拉黑" @click="friendsNone('拉黑')"/>
-  </template>
-  </van-swipe-cell>
-
-  <van-index-anchor index="B" />
-  <van-swipe-cell style="margin-bottom: .625rem;margin-left: .625rem;">
-    <van-image
-    round
-    width="2.5rem"
-    height="2.5rem"
-    fit="cover"
-    position="center"
-    src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
-    style="vertical-align: middle;"
-    />
-    <h4 style="display:inline-block;width: 80%;padding-right: 60%;color: black;margin-left: .625rem;">A朋友</h4>
-    <template #right>
-    <van-button square type="danger" text="删除" @click="friendsDel('删除')"/>
-    <van-button square type="primary" text="拉黑" @click="friendsNone('拉黑')"/>
-  </template>
-  </van-swipe-cell>
-  <van-swipe-cell style="margin-bottom: .625rem;margin-left: .625rem;">
-    <van-image
-    round
-    width="2.5rem"
-    height="2.5rem"
-    fit="cover"
-    position="center"
-    src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
-    style="vertical-align: middle;"
-    />
-    <h4 style="display:inline-block;width: 80%;padding-right: 60%;color: black;margin-left: .625rem;">A朋友</h4>
-    <template #right>
-    <van-button square type="danger" text="删除" @click="friendsDel('删除')"/>
-    <van-button square type="primary" text="拉黑" @click="friendsNone('拉黑')"/>
-  </template>
-  </van-swipe-cell>
-  <van-swipe-cell style="margin-bottom: .625rem;margin-left: .625rem;">
-    <van-image
-    round
-    width="2.5rem"
-    height="2.5rem"
-    fit="cover"
-    position="center"
-    src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
-    style="vertical-align: middle;"
-    />
-    <h4 style="display:inline-block;width: 80%;padding-right: 60%;color: black;margin-left: .625rem;">A朋友</h4>
-    <template #right>
-    <van-button square type="danger" text="删除" @click="friendsDel('删除')"/>
-    <van-button square type="primary" text="拉黑" @click="friendsNone('拉黑')"/>
-  </template>
-  </van-swipe-cell>
 
 </van-index-bar>
 
@@ -110,66 +28,66 @@ import axios from 'axios'
 import cnchar from 'cnchar'
 import { ref,onMounted } from 'vue'
 
-const friends = ref('')
+const friends = ref([1,2,3,4,5,6,7,8])
 
-const friendsLife = (value) => {
-    console.log(value);
-      axios.get('#').then(
-    response => {
-      if (response.status==200){
-              console.log(response);
-              alert('修改成功！')
-              friends = response.data
-              router.push( '/users' )
-            }else{
-              alert("请求报错")
-            }
-          },
-          error => {
-            console.log('提交数据失败，错误是：',error);
-          }
-        ),
-  error => {
-    console.log('提交失败，错误是：',error);
-  }
-}
-const friendsDel = (value) => {
-    console.log(value);
-      axios.get('').then(
-    response => {
-
-    }
-  ),
-  error => {
-    console.log('提交失败，错误是：',error);
-  }
-}
-const friendsNone= (value) => {
-    console.log(value);
-      axios.get('').then(
-    response => {
-
-    }
-  ),
-  error => {
-    console.log('提交失败，错误是：',error);
-  }
-}
-onMounted(() => {
-  axios.get('').then(
-    response => {
-
-    }
-  ),
-  error => {
-    console.log('提交失败，错误是：',error);
-  }
-})
-let indexList =  ref([])  //字母列表
-let data = ref(['是','的','我','啊','吧','从','额','给'])  //接收的后台数据
-let x =ref([]) 
-let y = ref(0)
-let o = ref(0)
+// const friendsLife = (value) => {
+//     console.log(value);
+//       axios.get('#').then(
+//     response => {
+//       if (response.status==200){
+//               console.log(response);
+//               alert('修改成功！')
+//               friends = response.data
+//               router.push( '/users' )
+//             }else{
+//               alert("请求报错")
+//             }
+//           },
+//           error => {
+//             console.log('提交数据失败，错误是：',error);
+//           }
+//         ),
+//   error => {
+//     console.log('提交失败，错误是：',error);
+//   }
+// }
+// const friendsDel = (value) => {
+//     console.log(value);
+//       axios.get('').then(
+//     response => {
+//
+//     }
+//   ),
+//   error => {
+//     console.log('提交失败，错误是：',error);
+//   }
+// }
+// const friendsNone= (value) => {
+//     console.log(value);
+//       axios.get('').then(
+//     response => {
+//
+//     }
+//   ),
+//   error => {
+//     console.log('提交失败，错误是：',error);
+//   }
+// }
+// onMounted(() => {
+//   axios.get('').then(
+//     response => {
+//
+//     }
+//   ),
+//   error => {
+//     console.log('提交失败，错误是：',error);
+//   }
+// })
+// let indexList =  ref([])  //字母列表
+// let data = ref(['是','的','我','啊','吧','从','额','给'])  //接收的后台数据
+// let x =ref([])
+// let y = ref(0)
+// let o = ref(0)
 
 //x[y]同一种字母的有多少种
 
